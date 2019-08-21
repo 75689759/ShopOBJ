1360a094fa642bc0d6ee48b0048786b5f4a605b5
wangning <75689759@qq.com>
2019-08-22 00:23:57 +0800

    备注

--- app/Http/Controllers/Admin/Products_List/ProductsController.php
+++ app/Http/Controllers/Admin/Products_List/ProductsController.php
@@ -12,24 +12,26 @@ use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
-	//后台产品类表模板
-    public function index(){
-    	return view('Admin.ProductsList.Products_List');
+    //后台产品类表模板
+    public function index()
+    {
+        return view('Admin.ProductsList.Products_List');
     }
 
     //后台产品添加模板
-    public function addshop(Request $request){
+    public function addshop(Request $request)
+    {
         // dd($request->hasFile('shoplogo'));
         if ($request->hasFile('shoplogo')) {
             $path = $request->file('shoplogo')->store(date('Ymd'));
-        }else{
+        } else {
             return back();
         }
 
-        if(empty($_POST['goods'] || $_POST['gnum'] || $_POST['store'] || $_POST['brand_id'] || $_POST['antistop'] || $_POST['unit'] || $_POST['original'])){
+        if (empty($_POST['goods'] || $_POST['gnum'] || $_POST['store'] || $_POST['brand_id'] || $_POST['antistop'] || $_POST['unit'] || $_POST['original'])) {
             return back();
             // return back()->with('error','请填写完整产品信息'); 
-        }else{
+        } else {
             $res = goods::create([
                 'goods' => $_POST['goods'],
                 'gnum' => $_POST['gnum'],
@@ -43,49 +45,54 @@ class ProductsController extends Controller
             // dd($request->input());
             return view('Admin.ProductsList.Products_List');
         }
-
     }
 
     //后台品牌管理模板
-    public function manage(){
+    public function manage()
+    {
         $res1 = brand::get()->toArray();
 
         $data = DB::select("select * from cates where ");
 
         $count = count($res1);
 
-    	return view('Admin.ProductsList.Products_Manage',['res1'=>$res1,'count'=>$count]);
+        return view('Admin.ProductsList.Products_Manage', ['res1' => $res1, 'count' => $count]);
     }
 
     //后台分类管理模板
-    public function category(){
+    public function category()
+    {
         $data = DB::select("select *,concat(path,id) as paths from cates order by paths asc");
-    	return view('Admin.ProductsList.Products_Category',['data'=>$data]);
+        return view('Admin.ProductsList.Products_Category', ['data' => $data]);
     }
 
     //后台添加分类模板
-    public function categoryAdd(){
+    public function categoryAdd()
+    {
 
         $data = DB::select("select *,concat(path,id) as paths from cates order by paths asc");
 
-    	return view('Admin.ProductsList.Products_Category_Add',['data'=>$data]);
+        return view('Admin.ProductsList.Products_Category_Add', ['data' => $data]);
     }
 
     //后台添加商品模板
-    public function pictureAdd(){
+    public function pictureAdd()
+    {
         $res1 = brand::get()->toArray();
 
-        return view('Admin.ProductsList.Products_Picture_Add',['res1'=>$res1]);
+        return view('Admin.ProductsList.Products_Picture_Add', ['res1' => $res1]);
     }
 
     //后台添加品牌模板
-    public function addBrand(){
+    public function addBrand()
+    {
         dump(session());
-        
+
         return view('Admin.ProductsList.Products_Add_Brand');
     }
 
-    public function manageadd(Request $request){
+    public function manageadd(Request $request)
+    {
 
         // dd(session('user')['id']);
 
@@ -101,13 +108,13 @@ class ProductsController extends Controller
 
         if ($request->hasFile('manage')) {
             $path = $request->file('manage')->store(date('Ymd'));
-        }else{
+        } else {
             return back();
         }
 
-        if(empty($_POST['bname'] || $_POST['number'] || $_POST['Country'] || $_POST['describe'])){
-            return back()->with('error','请填写完整品牌信息'); 
-        }else{
+        if (empty($_POST['bname'] || $_POST['number'] || $_POST['Country'] || $_POST['describe'])) {
+            return back()->with('error', '请填写完整品牌信息');
+        } else {
             $res = brand::create([
                 'shop_id' => $shopid,
                 'bname' => $_POST['bname'],
@@ -120,25 +127,26 @@ class ProductsController extends Controller
     }
 
     //后台添加分类模板
-    public function add(){
+    public function add()
+    {
 
         // var_dump($_POST);
 
         $cates = $_POST['cates'];
         $cname = $_POST['cname'];
 
-        if($cates == 0){
-            return back()->with('error','请选择分类');
-        }else if(empty($cname)){
-            return back()->with('error','请填写分类名');
-        }else if($cates == 0.5){
+        if ($cates == 0) {
+            return back()->with('error', '请选择分类');
+        } else if (empty($cname)) {
+            return back()->with('error', '请填写分类名');
+        } else if ($cates == 0.5) {
             cates::create([
                 'cname' => $cname,
                 'pid' => '0',
                 'path' => '0,',
             ]);
-            return back()->with('error','添加父分类成功');
-        }else if($cates != 0 || $cates != 0.5){
+            return back()->with('error', '添加父分类成功');
+        } else if ($cates != 0 || $cates != 0.5) {
             $dates = cates::find($cates);
 
             $path = $dates->path;
@@ -146,25 +154,26 @@ class ProductsController extends Controller
             cates::create([
                 'cname' => $cname,
                 'pid' => $cates,
-                'path' => $path.$cates.',',
+                'path' => $path . $cates . ',',
             ]);
-            return back()->with('error','添加子分类成功');
+            return back()->with('error', '添加子分类成功');
         }
     }
 
     //后台品牌启用禁用
-    public function state(Request $request){
+    public function state(Request $request)
+    {
         $id = $request->input('id');
         $state = $request->input('state');
 
-        if($state == '1'){
+        if ($state == '1') {
             $arr = brand::find($id);
             $res = $arr->update([
                 'state' => '2'
             ]);
 
             return back();
-        }elseif($state == '2'){
+        } elseif ($state == '2') {
             $arr = brand::find($id);
             $res = $arr->update([
                 'state' => '1'
@@ -175,12 +184,13 @@ class ProductsController extends Controller
     }
 
     //后台品牌删除
-    public function del(Request $request){
+    public function del(Request $request)
+    {
         // dump($request->input('id'));
 
         $id = $request->input('id');
 
-        $res = brand::where('id',$id)->delete();
+        $res = brand::where('id', $id)->delete();
 
         return back();
     }
